c239e6fd11f23a754218b55d9733baa26b071b9b
Gabriel Souza de Jesus <gabrielsjreal@gmail.com>
2019-06-08 15:17:29 -0300

    Configuração do Layout e do Banco de Dados

---
+++ LojaWeb/Aulas/Aula1/CasaDoCodigo/ApplicationContext.cs
@@ -0,0 +1,41 @@
+﻿using CasaDoCodigo.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // comandos para o Entity localizar a classe e gerar os comandos para a criação do banco de dados
+            // o  método 'HasKey' é para definir que a classe contém uma chave primária
+            modelBuilder.Entity<Produto>().HasKey(t => t.Id);
+
+            modelBuilder.Entity<Pedido>().HasKey(t => t.Id);
+            // o comando abaixo é para definir o relacionamento das tabelas, nesse caso abaixo por exemplo - n x n(muitos)
+            modelBuilder.Entity<Pedido>().HasMany(t => t.Itens).WithOne(t => t.Pedido);
+            // o comando abaixo é para definir o relacionamento das tabelas, nesse caso abaixo por exemplo - 1x1
+            modelBuilder.Entity<Pedido>().HasOne(t => t.Cadastro).WithOne(t => t.Pedido).IsRequired();
+
+            modelBuilder.Entity<ItemPedido>().HasKey(t => t.Id);
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Pedido);
+            modelBuilder.Entity<ItemPedido>().HasOne(t => t.Produto);
+
+            modelBuilder.Entity<Cadastro>().HasKey(t => t.Id);
+            modelBuilder.Entity<Cadastro>().HasOne(t => t.Pedido);
+        }
+    }
+}

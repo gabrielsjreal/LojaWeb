5ba2f5a76adaef2298be83044cf00f67903e05bd
Gabriel Souza de Jesus <gabrielsjreal@gmail.com>
2019-06-10 18:49:50 -0300

    Criação dos 'REPOSITORY' das classes do projeto

---
+++ LojaWeb/Aulas/Aula1/CasaDoCodigo/Repositories/BaseRepository.cs
@@ -0,0 +1,23 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public class BaseRepository<T> where T : BaseModel
+    {
+        protected readonly ApplicationContext contexto;
+        // comando para não permitir que elementos duplicados entrem no banco
+        protected readonly Microsoft.EntityFrameworkCore.DbSet<T> dbSet;
+
+        public BaseRepository(ApplicationContext contexto)
+        {
+            this.contexto = contexto;
+            // comando para não permitir que elementos duplicados entrem no banco
+            dbSet = contexto.Set<T>();
+        }
+
+    }
+}
--- LojaWeb/Aulas/Aula1/CasaDoCodigo/Repositories/ProdutoRepository.cs
+++ LojaWeb/Aulas/Aula1/CasaDoCodigo/Repositories/ProdutoRepository.cs
@@ -6,18 +6,15 @@ using System.Threading.Tasks;
 
 namespace CasaDoCodigo.Repositories
 {
-    public class ProdutoRepository : IProdutoRepository
+    public class ProdutoRepository : BaseRepository<Produto>, IProdutoRepository
     {
-        private readonly ApplicationContext contexto;
-
-        public ProdutoRepository(ApplicationContext contexto)
+        public ProdutoRepository(ApplicationContext contexto) : base(contexto)
         {
-            this.contexto = contexto;
         }
 
         public IList<Produto> GetProdutos()
         {
-            return contexto.Set<Produto>().ToList();
+            return dbSet.ToList();
         }
 
         public void SaveProdutos(List<Livro> livros)
@@ -25,7 +22,13 @@ namespace CasaDoCodigo.Repositories
             // o foreach é usado pasa instaciar os produtos já com os dados do arquivo .json
             foreach (var livro in livros)
             {
-                contexto.Set<Produto>().Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+                // o método Any(), verifica se a condição é verdadeira ou não
+                // comando para não permitir que elementos duplicados entrem no banco
+                if (!dbSet.Where(p => p.Codigo == livro.Codigo).Any())
+                {
+                    dbSet.Add(new Produto(livro.Codigo, livro.Nome, livro.Preco));
+                }
+              
             }
             // comando para salvar as alterações no banco
             contexto.SaveChanges();
